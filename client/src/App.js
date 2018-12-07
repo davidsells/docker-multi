a1194b0c8cee05ada9c5921eba476c569fb274c2
david <david@persistentdesigns.com>
2018-12-07 12:19:59 -0500

    changed title

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-	    <h1>Fib Calculator version 2</h1>
+	    <h1>Wave Calculator version 2</h1>
           </p>
           <a
             className="App-link"
